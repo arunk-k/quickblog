c6df67577ac9bd0537718ca902babfc4bf151966
Arun <arunkk1872002@gmail.com>
2025-09-19 15:47:28 +0530

    feat: add basic routing with Home, Blog pages and Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
 
 const App = () => {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
     </>
   )
 }
