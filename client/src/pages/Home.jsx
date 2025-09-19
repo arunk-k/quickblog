c267a398ddb36ba2224e325141f48032139e87c5
Arun <arunkk1872002@gmail.com>
2025-09-19 21:17:03 +0530

    feat: add Footer, BlogCard, Newsletter, and update BlogList with filtering

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import BlogList from "../components/BlogList"
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <Header />
-            <BlogList/>
+            <BlogList />
+            <Newsletter />
+            <Footer/>
         </>
     )
 }
